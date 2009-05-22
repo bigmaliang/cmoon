4dd7875fc6a944c9134b0d9151a06f966d4faf0c
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-05-22 05:04:24 +0000

    Former-commit-id: 970c600f2b4b54c48618a4361ab51a8fb956af05

--- walk/event/daemon/common.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-/* Define the common structures that are used throughout the whole server. */
-struct settings settings;
-struct stats stats;
-struct mevent *mevent;
-HDF *g_cfg;
