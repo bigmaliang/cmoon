5e82d1ab19ffc377dac48ab7074195a15f589d65
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-05-24 12:50:16 +0000

    build cmoon sleketon
    Former-commit-id: 1d1f9efba3a760e0d7e405146dc9d75a492d922b

---
+++ maketool/backupdir.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+PATH=/usr/local/bin:/usr/local/sbin:/bin:/usr/bin:/usr/sbin
+
+useage()
+{
+   echo "useage: $0 DIRS"
+   echo "example: $0 foo bar"
+   exit -1
+}
+
+
+if [ $# -lt 1 ]; then
+	useage
+fi
+
+TODAY=`date +%Y%m%d`
+PWD=`pwd`
+DIRNAME=$(basename "$PWD")
+
+DIRS=$*
+for dir in $DIRS
+do
+	make -C $dir clean
+done
+
+tar zcvf ${DIRNAME}_${TODAY}.tar.gz ${DIRS}
---
+++ maketool/jsglue.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+PATH=/usr/local/bin:/usr/local/sbin:/bin:/usr/bin:/usr/sbin
+
+useage()
+{
+   echo "useage: $0 -p JSMIN_PATH JSFILES"
+   echo "example: $0 -p ../maketool/jsmin foo.js bar.js"
+   exit -1
+}
+
+
+if [ $# -lt 2 ]; then
+	useage
+fi
+
+# process parameter
+while getopts 'p:' OPT; do
+	case $OPT in
+		p)
+			JSMIN="$OPTARG";;
+		?)
+			useage
+	esac
+done
+shift $(($OPTIND - 1))
+
+JSFILES=$*
+
+for file in $JSFILES
+do
+	${JSMIN} <$file
+done
