1d1f9efba3a760e0d7e405146dc9d75a492d922b
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-05-24 12:50:16 +0000

    build cmoon sleketon

---
+++ maketool/multimake.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+PATH=/usr/local/bin:/usr/local/sbin:/bin:/usr/bin:/usr/sbin
+
+useage()
+{
+   echo "useage: $0 -m METHOD DIRS"
+   echo "example: $0 -m clean foo bar"
+   exit -1
+}
+
+
+if [ $# -lt 1 ]; then
+	useage
+fi
+
+# process parameter
+while getopts ':m:' OPT; do
+	case $OPT in
+		m)
+			ACTION="$OPTARG";;
+		?)
+			useage
+	esac
+done
+shift $(($OPTIND - 1))
+
+DIRS=$*
+for dir in $DIRS
+do
+	echo "######### $ACTION $dir #########"
+	make -C $dir $ACTION
+done
