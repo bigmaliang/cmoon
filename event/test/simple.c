1a656e809adf3eb9495abd3224f2e6f8926911e8
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-05-31 10:59:00 +0000

    BUG string_init(zstra) ===> string_init(&zstra) TASK read NULL from config"
    Former-commit-id: 9be279e5e9acd14387354bfc516f1fba4cefbc38

--- event/test/simple.c
+++ event/test/simple.c
@@ -10,20 +10,23 @@ int main(int argc, char *argv[])
 {
 	unsigned long s_elapsed;
 	int times, suc, fai, err, busy;
-	char host[64];
+	char host[64], sql[1024];
 	uint32_t errcode;
 	int ret;
 
 	if (argc > 1) {
 		times = atoi(argv[1]);
 	} else {
-		printf("Usage: %s TIMES\n", argv[0]);
+		printf("Usage: %s TIMES [HOST] [SQL]\n", argv[0]);
 		return 1;
 	}
 	if (argc > 2) {
 		strncpy(host, argv[2], sizeof(host));
 	} else {
 		strcpy(host, "127.0.0.1");
+	}
+	if (argc > 3) {
+		strncpy(sql, argv[3], sizeof(sql));
 	}
 
 	mevent_t *evt = mevent_init();
@@ -33,19 +36,20 @@ int main(int argc, char *argv[])
 	}
 
 	mevent_add_udp_server(evt, host, 26010);
-#if 0
-	mevent_chose_plugin(evt, "db_community", REQ_CMD_SET, FLAGS_SYNC);
+#if 1
+	mevent_chose_plugin(evt, "db_community", REQ_CMD_SET, FLAGS_NONE);
 	mevent_add_array(evt, NULL, "sqls");
-	mevent_add_str(evt, "sqls", "1", "UPDATE myvideo.album SET title='没有杀毒' WHERE aid=11;");
-	mevent_add_str(evt, "sqls", "0", "UPDATE myvideo.album SET view_num = view_num+2 WHERE aid=11;");
+	if (!strcmp(sql, ""))
+		mevent_add_str(evt, "sqls", "0", "UPDATE myvideo.album SET title='没有杀毒' WHERE aid=11;");
+	else
+		mevent_add_str(evt, "sqls", "0", sql);
 #endif
 	
 	int i;
 	suc = fai = err = busy = 0;
 	timer_start();
-	char sql[1024];
 	for (i = 0; i < times; i++) {
-#if 1
+#if 0
 		mevent_chose_plugin(evt, "db_community", REQ_CMD_SET, FLAGS_NONE);
 		mevent_add_array(evt, NULL, "sqls");
 		sprintf(sql, "INSERT INTO eventcenter.events_3day(etype, fromuid, msg, eventtime) "
--- lib/mnl/mcs.h
+++ lib/mnl/mcs.h
@@ -7,38 +7,38 @@ __BEGIN_DECLS
 
 #define DIE_NOK_CGI(err)						\
 	if (err != STATUS_OK) {						\
-		STRING *zstra;	string_init(zstra);		\
-		nerr_error_traceback(err, zstra);		\
-		mtc_err("%s", zstra->buf);				\
-		string_clear(zstra);					\
+		STRING zstra;	string_init(&zstra);	\
+		nerr_error_traceback(err, &zstra);		\
+		mtc_err("%s", zstra.buf);				\
+		string_clear(&zstra);					\
 		cgi_neo_error(cgi, err);				\
 		exit(-1);								\
 	}
 
 #define DIE_NOK_MTL(err)						\
 	if (err != STATUS_OK) {						\
-		STRING *zstra;	string_init(zstra);		\
-		nerr_error_traceback(err, zstra);		\
-		mtc_err("%s", zstra->buf);				\
-		string_clear(zstra);					\
+		STRING zstra;	string_init(&zstra);	\
+		nerr_error_traceback(err, &zstra);		\
+		mtc_err("%s", zstra.buf);				\
+		string_clear(&zstra);					\
 		exit(-1);								\
 	}
 
 #define RETURN_NOK(err)							\
 	if (err != STATUS_OK) {						\
-		STRING *zstra;	string_init(zstra);		\
-		nerr_error_traceback(err, zstra);		\
-		mtc_err("%s", zstra->buf);				\
-		string_clear(zstra);					\
+		STRING zstra;	string_init(&zstra);	\
+		nerr_error_traceback(err, &zstra);		\
+		mtc_err("%s", zstra.buf);				\
+		string_clear(&zstra);					\
 		return;									\
 	}
 
 #define RETURN_V_NOK(err, v)					\
 	if (err != STATUS_OK) {						\
-		STRING *zstra;	string_init(zstra);		\
-		nerr_error_traceback(err, zstra);		\
-		mtc_err("%s", zstra->buf);				\
-		string_clear(zstra);					\
+		STRING zstra;	string_init(&zstra);	\
+		nerr_error_traceback(err, &zstra);		\
+		mtc_err("%s", zstra.buf);				\
+		string_clear(&zstra);					\
 		nerr_ignore(&err);						\
 		return v;								\
 	}
