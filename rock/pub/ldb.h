f88aae6df3b07caa99e5335a418fbef0f041ed01
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-06-01 09:33:21 +0000

    DEFECT use g_cfg instead of mcfg_xxx

--- rock/pub/ldb.h
+++ rock/pub/ldb.h
@@ -6,6 +6,7 @@ __BEGIN_DECLS
 
 #define DB_DSN			(mcfg_getvalue("dbdsn", "pgsql:dbname=mn_user host=localhost user=mner password=loveu"))
 #define DB_DSN_SYS		(mcfg_getvalue("dbdsn_sys", "pgsql:dbname=mn_sys host=localhost user=mner password=loveu"))
+#define DB_SYS			(hdf_get_value(g_cfg, "Db.Sys", "pgsql:dbname=mn_sys host=localhost user=mner password=loveu"))
 #define TABLE_RLS_USER	(mcfg_getvalue("table", "rls_user_4"))
 
 void ldb_opfinish(int ret, HDF *hdf, mdb_conn *conn,
