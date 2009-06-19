f250fdf1cd338ae52302031353b797e61f71e000
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-06-19 10:12:38 +0000

    BUG stop, copy, start, backup in storedb.sh

--- cdata/mtools/storedb.sh
+++ cdata/mtools/storedb.sh
@@ -4,8 +4,21 @@ TODAY=`date +%F`
 DATADIR=/data/qdbm/
 MTLDIR=/data/www/rtds/mtools/
 
-cp -f ${DATADIR}photo ${MTLDIR}dbs/photo_${TODAY} && ${MTLDIR}storedb -f ${MTLDIR}dbs/photo_${TODAY} -d n_photo
-cp -f ${DATADIR}user  ${MTLDIR}dbs/user_${TODAY}  && ${MTLDIR}storedb -f ${MTLDIR}dbs/user_${TODAY}  -d n_user
-cp -f ${DATADIR}blog  ${MTLDIR}dbs/blog_${TODAY}  && ${MTLDIR}storedb -f ${MTLDIR}dbs/blog_${TODAY}  -d n_blog
-cp -f ${DATADIR}video ${MTLDIR}dbs/video_${TODAY} && ${MTLDIR}storedb -f ${MTLDIR}dbs/video_${TODAY} -d n_video
+killall nmdb
+
+cp -f ${DATADIR}user  ${MTLDIR}dbs/user_${TODAY}  &&
+/usr/local/bin/nmdb -d /data/qdbm/user -t 26010 -u 26010 -c 30720 -o /var/log/nmdb.user &&
+${MTLDIR}storedb -f ${MTLDIR}dbs/user_${TODAY}  -d n_user
+
+cp -f ${DATADIR}photo ${MTLDIR}dbs/photo_${TODAY} &&
+/usr/local/bin/nmdb -d /data/qdbm/photo -t 26020 -u 26020 -c 30720 -o /var/log/nmdb.photo &&
+${MTLDIR}storedb -f ${MTLDIR}dbs/photo_${TODAY} -d n_photo
+
+cp -f ${DATADIR}video ${MTLDIR}dbs/video_${TODAY} &&
+/usr/local/bin/nmdb -d /data/qdbm/video -t 26030 -u 26030 -c 30720 -o /var/log/nmdb.video &&
+${MTLDIR}storedb -f ${MTLDIR}dbs/video_${TODAY} -d n_video
+
+cp -f ${DATADIR}blog  ${MTLDIR}dbs/blog_${TODAY}  &&
+/usr/local/bin/nmdb -d /data/qdbm/blog -t 26040 -u 26040 -c 30720 -o /var/log/nmdb.blog &&
+${MTLDIR}storedb -f ${MTLDIR}dbs/blog_${TODAY}  -d n_blog
 
