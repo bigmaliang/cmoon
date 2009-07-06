5dd000ec7b9868614b0d6023e4d2f6f364ccdad8
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-07-06 13:00:03 +0000

    BUG misc bug fix

--- rock/pager/admin/group/group.js
+++ rock/pager/admin/group/group.js
@@ -5,6 +5,31 @@ $(document).ready(function()
 		mode: "组属性",
 		status: "状态"
 	};
+	var opts_table = {
+		tbattr: "width='100%' border='0' cellspacing='1' cellpadding='0' class='list'",
+		tdattr: "class='td_border' align='center'"
+	};
+
+	function addMember(group)
+	{
+		$("#pid").val(group.id);
+	}
+
+	function rendRow(row, obj)
+	{
+		$(document).mntable(heads, row.groupinfo.members, opts_table).
+		appendTo(obj);
+	}
+
+	var opt_list = {
+		bindTo: "#grouplist",
+		title: "uri",
+		time: "intime",
+		titleClickName: "添加用户",
+		titleClickHref: "#memberadd",
+		titleClickCbk: addMember,
+		content: rendRow
+	};
 
 	function showGroup(page)
 	{
@@ -30,17 +55,12 @@ $(document).ready(function()
 				if (typeof mygroup != "undefined") {
 					mygroup.remove();
 				}
-				mygroup = $("<div></div>").appendTo($("#groups"));
-				$.each(data.groups, function(i, group)
-				{
-					$("<div>"+group.uri+"</div>").appendTo(mygroup);
-					$(document).mntable(heads, group.groupinfo.members,
-					opts_mntable).appendTo(mygroup);
-				});
+				mygroup = $("<div id='grouplist'></div>").appendTo($("#groups"));
+				$(document).mnlist(data.groups, opt_list);
 				$("#pagenav").mnpagenav({ttnum: data.ttnum, callback: showGroup});
 			},
 			error: function() {
-				alter("获取组列表失败");
+				alert("获取组列表失败");
 			}
 		});
 	}
