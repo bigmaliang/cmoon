af67f7cca80c745ea10eb2ed5e2020727ee72b9a
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-07-17 06:23:23 +0000

    BUG gbk ===> utf8 -_-

--- rock/mtls/data.sql
+++ rock/mtls/data.sql
@@ -3,20 +3,20 @@
 -- 0001' 0000'0001' 0000'0000 65792
 -- 0101' 0000'0001' 0000'0001 327937
 -- 1111' 0000'0111' 0000'0001 984833
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (0, 1001, 1, 0, 0, '/', ' ◊“≥'); -- can't direct access
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (0, 1001, 1, 0, 0, '/', 'È¶ñÈ°µ'); -- can't direct access
 
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 65792, 0, 0, 'admin', 'π‹¿Ì'); -- can't direct access
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 1, 0, 0, 'member', '”√ªß'); -- can't direct access
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 1, 0, 0, 'service', '∑˛ŒÒ'); -- can't direct access
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 327937, 0, 0, 'csc', '≤À –≥°');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 65792, 0, 0, 'admin', 'ÁÆ°ÁêÜ'); -- can't direct access
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 1, 0, 0, 'member', 'Áî®Êà∑'); -- can't direct access
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 1, 0, 0, 'service', 'ÊúçÂä°'); -- can't direct access
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (1, 1001, 327937, 0, 0, 'csc', 'ËèúÂ∏ÇÂú∫');
 
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 1, 'profile', '–≈œ¢');
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 255, 'account', '’ ∫≈');
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 4, 'file', 'Œƒº˛');
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 3, 'group', '◊È‘±');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 1, 'profile', '‰ø°ÊÅØ');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 255, 'account', 'Â∏êÂè∑');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 4, 'file', 'Êñá‰ª∂');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (2, 1001, 65792, 1, 3, 'group', 'ÁªÑÂëò');
 
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (3, 1001, 1, 1, 0, 'login', 'µ«¬º');
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (3, 1001, 1, 1, 0, 'logout', 'µ«≥ˆ');
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (3, 1001, 1, 1, 0, 'regist', '◊¢≤·');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (3, 1001, 1, 1, 0, 'login', 'ÁôªÂΩï');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (3, 1001, 1, 1, 0, 'logout', 'ÁôªÂá∫');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (3, 1001, 1, 1, 0, 'regist', 'Ê≥®ÂÜå');
 
-INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (4, 1001, 65792, 1, 0, 'action', '∂Ø◊˜');
+INSERT INTO fileinfo (pid, uid, mode, reqtype, lmttype, name, remark) VALUES (4, 1001, 65792, 1, 0, 'action', 'Âä®‰Ωú');
