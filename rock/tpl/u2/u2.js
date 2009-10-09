352be5fde99e9b1d94a454e5156d856082c4d447
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2009-10-09 10:25:03 +0000

    continue file appender

--- rock/tpl/u2/u2.js
+++ rock/tpl/u2/u2.js
@@ -1,17 +1,41 @@
 $(document).ready(function()
 {
 	$("button[rel=#fileappend]").overlay({closeOnClick: false});
+	$("#addfile").click(function()
+	{
+        if (!$(".VAL_APPEND").inputval()) {
+            return;
+        }
+        var data = {
+            filedesc: $("#filedesc").val()
+        };
+        if ($("#fileimg").val().length != 0) {
+            data.fileimg = $("#fileimg").val();
+        }
+        
+        $.post("/csc", data, successAdd, "json");
+	});
+
+    function successAdd(data) {
+        // TODO continue here
+        alert("添加成功");
+    }
+	
 	var uploader = new AjaxUpload("#imageadder",
 	{
 		action: '/csc',
 		name: 'imagename',
 		data: {op: 'add', tp: 'imageadd'},
-        responseType: "json",
-		autoSubmit: false,
-        onChange: replaceImage
+		responseType: "json",
+		autoSubmit: true,
+		onChange: replaceImage
 	});
 
-    function replaceImage() {
-        alert("change");
-    }
+	function replaceImage(file, ext) {
+		if (! (ext && /^(jpg|png|jpeg|gif)$/.test(ext))) {
+			alert("请选择图片");
+			return false;
+		}
+		return true;
+	}
 });
