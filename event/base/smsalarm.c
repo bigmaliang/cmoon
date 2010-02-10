2a1df57ed07915612238e14ec5ce1dae99d14086
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2010-02-10 09:45:28 +0000

    event - remove libmevent_server.so bug fix.  smsalarm change
    Former-commit-id: c00dfa45c46854aa3c33f86327e10e5d5c7aee76

--- event/base/smsalarm.c
+++ event/base/smsalarm.c
@@ -23,7 +23,7 @@
 void smsalarm_msg(char *msg)
 {
 	fdb_t *db;
-	char content[100];
+	//char content[100];
 	int ret;
 	
 	ret = fdb_init_long(&db, SMSA_CFG_IP, SMSA_CFG_USER,
@@ -37,17 +37,18 @@ void smsalarm_msg(char *msg)
 	if (node != NULL) node = hdf_obj_child(node);
 
 	while (node != NULL) {
-		memset(content, 0x0, sizeof(content));
+		//memset(content, 0x0, sizeof(content));
 		memset(db->sql, 0x0, sizeof(db->sql));
 		
-		snprintf(content, sizeof(content), "%s|%s", hdf_obj_value(node), msg);
+		//snprintf(content, sizeof(content), "%s|%s", hdf_obj_value(node), msg);
 		snprintf(db->sql, sizeof(db->sql),
-				 "INSERT INTO pei5_smssend (smsContent) VALUES ('%s')", content);
+				 "INSERT INTO monitor_smssend (smsSendTo, smsContent) VALUES ('%s', '%s')",
+				 hdf_obj_value(node), msg);
 		ret = fdb_exec(db);
 		if (ret != RET_DBOP_OK) {
-			wlog("exec %s failure %s\n", content, fdb_error(db));
+			wlog("exec %s failure %s\n", msg, fdb_error(db));
 		} else {
-			wlog("%s alarm ok\n", content);
+			wlog("%s alarm ok\n", msg);
 		}
 		
 		node = hdf_obj_next(node);
