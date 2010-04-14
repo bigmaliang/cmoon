1338de853b2e7651b88f3bfc5c97a27270ccb1cb
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2010-04-14 01:22:20 +0000

    deliver - user github JSF
    Former-commit-id: 7708e57f0b52d1bb673539aafd78a95a6fbc7b3a

--- deliver/mtools/fkq_mod_bat.sh
+++ deliver/mtools/fkq_mod_bat.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
+# $fkq_mod_bat.sh open/close [uin.txt]
 
-USERS="
-3		 
-6		 
-775393 
-1855123
-1671006
-778444 
-1916312
-2046576
-1864902
-1593207
-1751745
-"
+TODAY=`date +%F`
+TMPFILE="/usr/local/ape/mtools/fkq_open_$TODAY.txt"
+
+if [ $# -gt 1 ]; then
+	TMPFILE=$2
+else
+	echo -n > $TMPFILE
+	echo "SELECT userid FROM user_info WHERE bonus>1000 AND fkq_level != 1;" | /usr/local/mysql/bin/mysql -h 192.168.1.23 -u sq_mysq1_count -psq1_m^sq1_ssap -P3063 -Dhome -N > $TMPFILE
+fi
+
+USERS=`cat $TMPFILE`
 
 if [ "$1" = "open" ]; then
-	for user in $USERS; do
-		/usr/local/bin/uic 1 3002 $user -1 9 -1 1
-	done
+    for user in $USERS; do
+        /usr/local/bin/uic 1 3002 $user -1 9 -1 1
+    done
 elif [ "$1" = "close" ]; then
-	for user in $USERS; do
-		/usr/local/bin/uic 1 3002 $user -1 9 -1 0
-	done
+    for user in $USERS; do
+        /usr/local/bin/uic 1 3002 $user -1 9 -1 0
+    done
 else
-	echo "unknown operation"
+    echo "unknown operation"
 fi
