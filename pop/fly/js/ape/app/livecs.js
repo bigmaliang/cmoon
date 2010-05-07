b595962212895b58b4e977dc8ddc8fe371ff069d
bigmaliang <bigmaliang@08b3e164-4678-11de-9a4c-a76526a193c6>
2010-05-07 09:56:43 +0000

    pop - lcs client js most partly ok

--- pop/fly/js/ape/app/livecs.js
+++ pop/fly/js/ape/app/livecs.js
@@ -11,45 +11,77 @@ function liveCS(ape, debug) {
 		}
         ape.lcsaname = opts.aname || document.domain;
         ape.lcsPipeName = "livecspipe_" + ape.lcsaname;
-        ape.lcsPipe = null;
+		this.publicPipe = null;
+		this.currentPipe = null;
 
+		ape.onRaw("ident", this.rawLcsIdent);
         ape.onRaw("lcsdata", this.rawLcsData);
+		
+		ape.onError('110', ape.clearSession());
+		ape.onError('111', ape.clearSession());
+		ape.onError('112', ape.clearSession());
+		ape.onError('113', ape.clearSession());
+
         ape.addEvent("userJoin", this.createUser);
         ape.addEvent("userLeft", this.deleteUser);
         ape.addEvent("multiPipeCreate", this.pipeCreate);
 
         ape.addEvent("load", this.start);
-		ape.onRaw("ident", this.rawIdent);
     };
 
     this.start = function() {
-		ape.start({'uin': ape.lcsuname});
+		var opt = {'sendStack': false, 'request': 'stack'};
+		ape.start({'uin': ape.lcsuname}, opt);
+		if (ape.options.restore) {
+			ape.getSession('currentPipe', function(resp) {
+							   this.setCurrentPipe(resp.data.sessions.currentPipe);
+						   }, opt);
+		} else {
+			ape.request.send("LCS_JOIN", {'aname': ape.lcsaname}, opt);
+		}
+		ape.request.stack.send();
     };
 
-	this.rawIdent = function() {
-        ape.request.send("LCS_JOIN", {'aname': ape.lcsaname}, true);
+	this.setCurrentPipe = function(pubid) {
+		this.currentPipe = ape.getPipe(pubid);
+		ape.setSession({'currentPipe': pubid});
+	};
+
+	this.getCurrentPipe = function() {
+		return this.currentPipe;
 	};
 
     this.pipeCreate = function(pipe, options) {
         if (pipe.properties.name.toLowerCase() == ape.lcsPipeName) {
-            ape.lcsPipe = pipe;
-        }
+			this.publicPipe = pipe;
+			if (!this.getCurrentPipe()) {
+				this.setCurrentPipe(pipe.getPubid());
+			}
+		}
     };
 
     this.createUser = function(user, pipe) {
-        if (pipe.properties.name.toLowerCase() == ape.lcsPipeName) {
-			//bmoon.lcs.userVisit(ape, user.properties.uin);
-        }
+		if (pipe.properties.isadmin) {
+			this.setCurrentPipe(pipe.getPubid());
+		}
     };
 
     this.deleteUser = function(user, pipe) {
-        if (pipe.properties.name.toLowerCase() == ape.lcsPipeName) {
-			//bmoon.lcs.userAway(user.properties.uin);
-        }
+		if (pipe.properties.isadmin) {
+			this.setCurrentPipe(this.publicPipe.getPubid());
+		}
     };
 
+	this.rawLcsIdent = function(raw, pipe) {
+		var jid = raw.data.user.properties.jid;
+		// send LCS_VISIT only on session restore
+		if (jid && ape.options.restore) {
+			ape.request.send("LCS_VISIT", {'jid': jid, 'url': window.location.href, 'title': window.title});
+		}
+	};
+
     this.rawLcsData = function(raw, pipe) {
-        if (pipe.properties.name.toLowerCase() == ape.lcsPipeName) {
+        if (pipe == this.getCurrentPipe()) {
             var msg = unescape(raw.data.msg);
             var uin = raw.data.from.properties.uin;
             var tm = Date(eval(raw.time)).match(/\d{1,2}:\d{1,2}:\d{1,2}/)[0];
