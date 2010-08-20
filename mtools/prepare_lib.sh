1e4c5df900064d514adee215578afb124b725cf6
bigml <bigmaliang@gmail.com>
2010-08-20 10:31:08 +0800

    ksa & pop - misc change
    Former-commit-id: 08e939ab4afe7ddc34a6f42bde9ebf44fdd04591

--- mtools/prepare_lib.sh
+++ mtools/prepare_lib.sh
@@ -7,8 +7,8 @@ sudo apt-get install libmysqlclient-dev libpq-dev libsqlite3-dev
 # Other Linux distribution
 if [ 1 == 2 ]; then
 	PREFIX="/usr/local"
-	SRCS="http://www.clearsilver.net/downloads/clearsilver-0.10.5.tar.gz http://www.fastcgi.com/dist/fcgi.tar.gz http://oss.metaparadigm.com/json-c/json-c-0.9.tar.gz http://download.tangent.org/libmemcached-0.38.tar.gz http://blitiri.com.ar/p/nmdb/files/0.22/nmdb-0.22.tar.gz http://qdbm.sourceforge.net/qdbm-1.8.77.tar.gz"
-	DIRS="clearsilver-0.10.5 fcgi-2.4.0 json-c-0.9 libmemcached-0.38 nmdb-0.22 qdbm-1.8.77"
+	SRCS="http://www.clearsilver.net/downloads/clearsilver-0.10.5.tar.gz http://www.fastcgi.com/dist/fcgi.tar.gz http://oss.metaparadigm.com/json-c/json-c-0.9.tar.gz http://launchpad.net/libmemcached/1.0/0.43/+download/libmemcached-0.43.tar.gz http://blitiri.com.ar/p/nmdb/files/0.22/nmdb-0.22.tar.gz http://qdbm.sourceforge.net/qdbm-1.8.77.tar.gz"
+	DIRS="clearsilver-0.10.5 fcgi-2.4.0 json-c-0.9 libmemcached-0.43 nmdb-0.22 qdbm-1.8.77"
 
 	cd ${PREFIX}/src/
 	
@@ -27,6 +27,8 @@ if [ 1 == 2 ]; then
 			./configure --prefix=${PREFIX} --disable-wdb --disable-perl --disable-ruby --disable-java --disable-python && make && make installl
 		elif [ $dir == "nmdb-0.22" ]; then
 			cd libnmdb && make && make install && cd -
+		elif [ $dir == "libmemcached-0.43" ]; then
+			./configure --disable-64bit CFLAGS="-O3 -march=i686" && make && make install
 		else
 			./configure --prefix=${PREFIX} && make && make install
 		fi
