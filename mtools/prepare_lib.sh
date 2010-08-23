6b79a42dbfa04a416b3be8904fe684a1cc100b24
bigml <bigmaliang@gmail.com>
2010-08-23 16:07:25 +0800

    lib - clearsilver use the google svn version
    Former-commit-id: 6937c58e19da0b2b0f5d6413e5871e2169808818

--- mtools/prepare_lib.sh
+++ mtools/prepare_lib.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
 # Debian system
-sudo apt-get install clearsilver-dev libfcgi-dev libjson0-dev libmemcached-dev libqdbm-dev libevent-dev
+
+cd /usr/local/src &&
+svn checkout http://clearsilver.googlecode.com/svn/trunk/ clearsilver
+cd clearsilver &&
+svn checkout http://streamhtmlparser.googlecode.com/svn/trunk/ streamhtmlparser
+cd streamhtmlparser && ./configure && make && make install
+cd ../ && ./autogen.sh --disable-wdb --disable-perl --disable-ruby --disable-java --disable-python && mkdir libs
+make
+make install
+
+
+sudo apt-get install libfcgi-dev libjson0-dev libmemcached-dev libqdbm-dev libevent-dev
 sudo apt-get install libmysqlclient-dev libpq-dev libsqlite3-dev
 
 # Other Linux distribution
 if [ 1 == 2 ]; then
 	PREFIX="/usr/local"
-	SRCS="http://www.clearsilver.net/downloads/clearsilver-0.10.5.tar.gz http://www.fastcgi.com/dist/fcgi.tar.gz http://oss.metaparadigm.com/json-c/json-c-0.9.tar.gz http://launchpad.net/libmemcached/1.0/0.43/+download/libmemcached-0.43.tar.gz http://blitiri.com.ar/p/nmdb/files/0.22/nmdb-0.22.tar.gz http://qdbm.sourceforge.net/qdbm-1.8.77.tar.gz"
+	SRCS="http://www.fastcgi.com/dist/fcgi.tar.gz http://oss.metaparadigm.com/json-c/json-c-0.9.tar.gz http://launchpad.net/libmemcached/1.0/0.43/+download/libmemcached-0.43.tar.gz http://blitiri.com.ar/p/nmdb/files/0.22/nmdb-0.22.tar.gz http://qdbm.sourceforge.net/qdbm-1.8.77.tar.gz"
 	DIRS="clearsilver-0.10.5 fcgi-2.4.0 json-c-0.9 libmemcached-0.43 nmdb-0.22 qdbm-1.8.77"
 
 	cd ${PREFIX}/src/
@@ -23,9 +34,7 @@ if [ 1 == 2 ]; then
 	for dir in $DIRS; do
 		cd $dir
 		
-		if [ $dir == "clearsilver-0.10.5" ]; then
-			./configure --prefix=${PREFIX} --disable-wdb --disable-perl --disable-ruby --disable-java --disable-python && make && make installl
-		elif [ $dir == "nmdb-0.22" ]; then
+		if [ $dir == "nmdb-0.22" ]; then
 			cd libnmdb && make && make install && cd -
 		elif [ $dir == "libmemcached-0.43" ]; then
 			./configure --disable-64bit CFLAGS="-O3 -march=i686" && make && make install
