2b1805c51ff0e8f3cfa17e72198dd58b002b2a1d
bigml <bigmaliang@gmail.com>
2010-09-02 18:11:29 +0800

    mnl - mjson_output_hdf improve
    Former-commit-id: e97ba4d060fa663f4913e32b9f2511674ef42354

--- lib/mnl/mjson.c
+++ lib/mnl/mjson.c
@@ -39,59 +39,121 @@ void mjson_asm_objs(HDF *hdf, struct json_object *out)
 
 	HDF *chi, *num;
 	struct json_object *arr, *jso, *item;
-	char *val;
+	char *val, *type;
 	arr = jso = NULL;
 
-	/* process child node */
-	chi = hdf_obj_child(hdf);
-	if (chi != NULL) {
-		num = hdf_get_obj(hdf, "0");
-		if (num != NULL) {
-			if (hdf_obj_child(num) != NULL) {
+	hdf = hdf_obj_child(hdf);
+
+	while (hdf) {
+		if ((val = hdf_obj_value(hdf)) != NULL) {
+			type = mutil_obj_attr(hdf, "type");
+			if (type != NULL && !strcmp(type, "int")) {
+				jso = json_object_new_int(atoi(val));
+			} else {
+				jso = json_object_new_string(val);
+			}
+			json_object_object_add(out, hdf_obj_name(hdf), jso);
+		}
+
+		if (hdf_obj_child(hdf) != NULL) {
+			num = hdf_get_obj(hdf, "0");
+			if (num != NULL) {
 				/* child is array */
 				arr = json_object_new_array();
-				while (num != NULL ) {
-					item = json_object_new_object();
+
+				while (num !=NULL) {
+					jso = json_object_new_object();
+
 					if (hdf_obj_child(num) != NULL) {
-						mjson_asm_objs(hdf_obj_child(num), item);
+						mjson_asm_objs(num, jso);
 					} else {
 						val = hdf_obj_value(num);
 						if (val != NULL) {
-							char *type = mutil_obj_attr(num, "type");
+							type = mutil_obj_attr(num, "type");
 							if (type != NULL && !strcmp(type, "int")) {
 								jso = json_object_new_int(atoi(val));
 							} else {
 								jso = json_object_new_string(val);
 							}
-							json_object_object_add(item, hdf_obj_name(num), jso);
 						}
 					}
-					json_object_array_add(arr, item);
+
+					json_object_array_add(arr, jso);
+					
 					num = hdf_obj_next(num);
 				}
 				json_object_object_add(out, hdf_obj_name(hdf), arr);
-				goto next_obj;
+			} else {
+				jso = json_object_new_object();
+				mjson_asm_objs(hdf, jso);
+				json_object_object_add(out, hdf_obj_name(hdf), jso);
 			}
 		}
-		jso = json_object_new_object();
-		mjson_asm_objs(chi, jso);
-		json_object_object_add(out, hdf_obj_name(hdf), jso);
+		
+		hdf = hdf_obj_next(hdf);
 	}
+}
 
-	/* process leaf hdf */
-	val = hdf_obj_value(hdf);
-	if (val != NULL) {
-		char *type = mutil_obj_attr(hdf, "type");
-		if (type != NULL && !strcmp(type, "int")) {
-			jso = json_object_new_int(atoi(val));
-		} else {
-			jso = json_object_new_string(val);
+void mjson_asm_arrs(HDF *hdf, struct json_object *out)
+{
+	if (hdf == NULL)
+		return;
+
+	HDF *chi, *num;
+	struct json_object *arr, *jso, *item;
+	char *val, *type;
+	arr = jso = NULL;
+
+	hdf = hdf_obj_child(hdf);
+
+	while (hdf) {
+		if ((val = hdf_obj_value(hdf)) != NULL) {
+			type = mutil_obj_attr(hdf, "type");
+			if (type != NULL && !strcmp(type, "int")) {
+				jso = json_object_new_int(atoi(val));
+			} else {
+				jso = json_object_new_string(val);
+			}
+			json_object_array_add(out, jso);
 		}
-		json_object_object_add(out, hdf_obj_name(hdf), jso);
-	}
 
-next_obj:
-	mjson_asm_objs(hdf_obj_next(hdf), out);
+		if (hdf_obj_child(hdf) != NULL) {
+			num = hdf_get_obj(hdf, "0");
+			if (num != NULL) {
+				/* child is array */
+				arr = json_object_new_array();
+
+				while (num !=NULL) {
+					jso = json_object_new_object();
+
+					if (hdf_obj_child(num) != NULL) {
+						mjson_asm_objs(num, jso);
+					} else {
+						val = hdf_obj_value(num);
+						if (val != NULL) {
+							type = mutil_obj_attr(num, "type");
+							if (type != NULL && !strcmp(type, "int")) {
+								jso = json_object_new_int(atoi(val));
+							} else {
+								jso = json_object_new_string(val);
+							}
+						}
+					}
+
+					json_object_array_add(arr, jso);
+					
+					num = hdf_obj_next(num);
+				}
+				json_object_array_add(out, arr);
+			} else {
+				jso = json_object_new_object();
+				mjson_asm_objs(hdf, jso);
+				json_object_array_add(out, jso);
+			}
+		}
+		
+		hdf = hdf_obj_next(hdf);
+	}
 }
 
 void mjson_output_hdf(HDF *hdf, time_t second)
@@ -102,16 +164,20 @@ void mjson_output_hdf(HDF *hdf, time_t second)
 	
 	NEOERR *err = cgiwrap_writef("Content-Type: text/html; charset=UTF-8\r\n\r\n");
 	if (err != STATUS_OK) nerr_ignore(&err);
-	HDF *ohdf = hdf_get_child(hdf, PRE_OUTPUT);
+	HDF *ohdf = hdf_get_obj(hdf, PRE_OUTPUT);
 	if (ohdf == NULL) {
 		return;
 	}
 
-	struct json_object *out = json_object_new_object();
-	if (out == NULL)
-		return;
+	struct json_object *out;
+	if (hdf_get_obj(ohdf, "0")) {
+		out = json_object_new_array();
+		mjson_asm_arrs(ohdf, out);
+	} else {
+		out = json_object_new_object();
+		mjson_asm_objs(ohdf, out);
+	}
 
-	mjson_asm_objs(ohdf, out);
 	err = cgiwrap_writef("%s\n", json_object_to_json_string(out));
 	if (err != STATUS_OK) nerr_ignore(&err);
 	json_object_put(out);
@@ -124,16 +190,20 @@ void mjson_execute_hdf(HDF *hdf, char *cb, time_t second)
 	
 	NEOERR *err = cgiwrap_writef("Content-Type: text/html; charset=UTF-8\r\n\r\n");
 	if (err != STATUS_OK) nerr_ignore(&err);
-	HDF *ohdf = hdf_get_child(hdf, PRE_OUTPUT);
+	HDF *ohdf = hdf_get_obj(hdf, PRE_OUTPUT);
 	if (ohdf == NULL) {
 		return;
 	}
 
-	struct json_object *out = json_object_new_object();
-	if (out == NULL)
-		return;
+	struct json_object *out;
+	if (hdf_get_obj(ohdf, "0")) {
+		out = json_object_new_array();
+		mjson_asm_arrs(ohdf, out);
+	} else {
+		out = json_object_new_object();
+		mjson_asm_objs(ohdf, out);
+	}
 
-	mjson_asm_objs(ohdf, out);
 	cgiwrap_writef("%s(%s);\n", cb, json_object_to_json_string(out));
 	if (err != STATUS_OK) nerr_ignore(&err);
 	json_object_put(out);
