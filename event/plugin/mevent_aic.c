f42be54413f75036b2ec72eb85e94c1f22be29d7
bigml <bigmaliang@gmail.com>
2010-12-29 16:44:01 +0800

    pop - process REP_ERR_XXX more comfortable
    Former-commit-id: 8c621bb82a4f89887e47e5a34cfd42ca4bb1e2b6

--- event/plugin/mevent_aic.c
+++ event/plugin/mevent_aic.c
@@ -49,12 +49,15 @@ static NEOERR* aic_cmd_appinfo(struct queue_entry *q, struct cache *cd, mdb_conn
 	} else {
 		hdf_set_value(q->hdfsnd, "pname", aname);
 		MDB_QUERY_RAW(db, "appinfo", APPINFO_COL, "aid=%d", NULL, aid);
+		if (nerr_handle(&err, NERR_NOT_FOUND))
+			return nerr_raise(REP_ERR_NREGIST, "%s %d hasn't regist", aname, aid);
 		err = mdb_set_row(q->hdfsnd, db, APPINFO_COL, NULL);
 		if (err != STATUS_OK) return nerr_pass(err);
 		pid = hdf_get_int_value(q->hdfsnd, "pid", 0);
 		if (pid != 0) {
 			MDB_QUERY_RAW(db, "appinfo", "aname", "aid=%d", NULL, pid);
 			err = mdb_set_row(q->hdfsnd, db, "pname", NULL);
+			nerr_handle(&err, NERR_NOT_FOUND);
 			if (err != STATUS_OK) return nerr_pass(err);
 		}
 		MDB_QUERY_RAW(db, "appinfo", "COUNT(*)+1 AS numuser", "pid=%d",
