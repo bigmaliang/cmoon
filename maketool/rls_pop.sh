e480b3b3422a5912e794f45f5c5ccd3de1eae131
bigml <bigmaliang@gmail.com>
2011-04-12 15:21:51 +0800

    pop - statOnly ok
    Former-commit-id: d64089158b6337467c091bc2044a2a0053653cee

--- maketool/rls_pop.sh
+++ maketool/rls_pop.sh
@@ -23,9 +23,15 @@ tar zcvf ${TODAY}_pop.tar.gz /usr/local/lib/mevent_*.so \
 	/usr/local/bin/mevent /usr/local/bin/hb /etc/mevent/ \
 	/usr/local/moon/
 
+echo "\n******** RESTORE ********"
+sleep 2
+make clean
+make
+
 echo "\n******** DONE ********"
 echo "\n** please remember modify "
 echo "ALTER USER lcser with password 'xxxxxx';"
 echo "/usr/local/moon/pop/config.hdf (tclevel, pass)"
+echo "/usr/local/moon/pop/mtls/view_mtls.sql"
 echo "/usr/local/moon/pop/mtls/conf/*.conf"
 echo "/etc/mevent/server.hdf (tclevel, pass)"
