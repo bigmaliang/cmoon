fa9baef3365cdecc5cbebac5c696df80b6bf9b87
bigml <bigmaliang@gmail.com>
2012-04-10 17:47:23 +0800

    dida - /trace skeleton and type:pageview in ok
    Former-commit-id: 16b3912d384c72b52f4d970ea4dfd58a485617fe

--- event/plugin/mevent_skeleton.h
+++ event/plugin/mevent_skeleton.h
@@ -1,11 +1,11 @@
 #ifndef __MEVENT_SKELETON_H__
 #define __MEVENT_SKELETON_H__
 
-#define PREFIX_SKLE     "Skele"
+#define PREFIX_SKELETON     "Skeleton"
 
 enum {
-    REQ_CMD_SKGET = 1001,
-    REQ_CMD_SKSET
+    REQ_CMD_SKELETON_GET = 1001,
+    REQ_CMD_SKELETON_ADD
 };
 
 #endif    /* __MEVENT_SKELETON_H__ */
