bab59bd35b58dbac9d54d4533a2024182eebbadc
bigml <bigmaliang@gmail.com>
2012-11-23 18:16:05 +0800

    doc - add mg.c
    Former-commit-id: ac08f8ddd150d4bd4267223e708bd401800fa7c1

---
+++ doc/demo/mg.c
@@ -0,0 +1,23 @@
+#include "mheads.h"
+
+HDF *g_cfg = NULL;
+
+int main(int argc, char **argv, char **envp)
+{
+    NEOERR *err;
+    mmg_conn *db;
+
+    err = mmg_init("172.10.7.211", 27017, &db);
+    OUTPUT_NOK(err);
+    
+    err = mmg_string_update(db, "ml.count", "{'foo': 'bar'}", "{'foo': 'barr', 'xxx': 1}");
+    OUTPUT_NOK(err);
+
+    char *error = NULL;
+    //mongo_sync_cmd_get_last_error(db->con, "ml.count", &error);
+    printf("update error %d, %s", 1<<2, error);
+
+    mmg_destroy(db);
+
+    return 0;
+}
